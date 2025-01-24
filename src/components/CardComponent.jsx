1e55ad7fb1aa449035fc5940456c37881a9baa3a
giuse <giuseppedefilippo03@gmail.com>
2025-01-24 17:13:09 +0100

    chiamata axios per i film

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,16 +1,15 @@
-export default function CardComponent({ title, director, genre, releasey, abstract, image, created, updated }) {
+export default function CardComponent({ title, director, genre, year, abstract, image, created_at, updated_at }) {
     return (
-        <div className="card" style={{ width: "18remò" }}>
-            < img src="" className="card-img-top" alt="" />
+        <div className="card" style={{ width: "18rem" }}>
+            < img src={image} className="card-img-top" alt="" />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">director</p>
-                <p className="card-text">genre</p>
-                <p className="card-text">release year</p>
-                <p className="card-text">abstract</p>
-                <p className="card-text">created_at</p>
-                <p className="card-text">updated_at</p>
-
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">director: {director}</p>
+                <p className="card-text"> genre: {genre}</p>
+                <p className="card-text">release {year}</p>
+                <p className="card-text">abstract: {abstract} </p>
+                <p className="card-text">created_at: {created_at} </p>
+                <p className="card-text">updated_at: {updated_at} </p>
             </div>
         </div >
     )
--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,7 +1,40 @@
 import CardComponent from "./CardComponent";
-
+import { useEffect, useState } from "react";
+import axios from "axios"
 export default function MainComponent() {
+    const [movies, setMovies] = useState([]) //si memorizzano i dati
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/movie")
+            .then((response) => {
+                setMovies(response.data.film);
+            })
+            .catch((error) => {
+                console.error("errore", error);
+            });
+    }, []);
+
+
+
+
     return (
-        <CardComponent />
-    )
+        <div>
+            {movies.length === 0 ? (
+                <div>non ci sono film</div>
+            ) : (
+                movies.map((movie) => (
+                    <CardComponent
+                        key={movie.id}
+                        title={movie.title}
+                        director={movie.director}
+                        genre={movie.genre}
+                        year={movie.release_year}
+                        abstract={movie.abstract}
+                        created_at={movie.created_at}
+                        updated_at={movie.updated_at}
+                    />
+                ))
+            )}
+        </div>
+    );
 }
