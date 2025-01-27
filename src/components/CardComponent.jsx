0ec14133449e05508454d4e0d372bd6a869e9fad
giuse <giuseppedefilippo03@gmail.com>
2025-01-27 18:09:57 +0100

    sparisce tutto quando si va us mobile e se si clicca esce tutto

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -3,13 +3,13 @@ export default function CardComponent({ title, director, genre, year, abstract, 
         <div className="card" style={{ width: "18rem" }}>
             <img src={image} className="card-img-top" alt={title} />
             <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">Director: {director}</p>
-                <p className="card-text">Genre: {genre}</p>
-                <p className="card-text">Release: {year}</p>
-                <p className="card-text">Abstract: {abstract}</p>
-                <p className="card-text">Created At: {created_at}</p>
-                <p className="card-text">Updated At: {updated_at}</p>
+                <h5 className="card-title  d-none d-md-block">{title}</h5>
+                <p className="card-text  d-none d-sm-block">Director: {director}</p>
+                <p className="card-text  d-none d-sm-block">Genre: {genre}</p>
+                <p className="card-text  d-none d-sm-block">Release: {year}</p>
+                <p className="card-text  d-none d-sm-block">Abstract: {abstract}</p>
+                <p className="card-text  d-none d-sm-block">Created At: {created_at}</p>
+                <p className="card-text  d-none d-sm-block">Updated At: {updated_at}</p>
             </div>
         </div>
     );
